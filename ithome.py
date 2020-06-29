52a0e48b5ff415a95cd37a48a25eedb2832f96ee
soy-A <skql1502@gmail.com>
2020-06-29 16:08:43 +0900

    update ithome.py

--- ithome.py
+++ ithome.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
-driver = webdriver.Chrome("/usr/local/bin/chromedriver") #본인의 chrome driver 주소
 import json
-from collections import OrderedDict
 import threading
+driver = webdriver.Chrome(executable_path = '/workspace/chromedriver', options = options)
+
+from collections import OrderedDict
 
 file_data = OrderedDict()
 
@@ -11,7 +12,6 @@ def notice():
 
 	data = {}
 	for i in range(5):
-		# xpath '//*[@id="recentBbsArtclObj_681_1050"]/li[%d]/a'안의 %d값을 1씩 올려 순차적으로 공지를 가져온다.
 		text = driver.find_element_by_xpath('//*[@id="recentBbsArtclObj_681_1050"]/li[%d]/a'%(i+1)).text
 		title = text[:-6]
 		date = text[-5:]
